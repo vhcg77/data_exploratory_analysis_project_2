3487041a6b6b144e4cc24ca62f5accaebc330f82
vhcg77 <vhcg77@gmail.com>
2018-02-26 01:13:43 -0300

    Third commit

--- Plot2.R
+++ Plot2.R
@@ -5,3 +5,11 @@ SCC <- readRDS("c:/2018/ejemplos/pm2.5/Source_Classification_Code.rds")
 
 head(NEI)
 head(SCC)
+
+Total_emission_Baltimore <- subset(NEI, fips == "24510")
+Total_emission_by_year_Baltimore <- aggregate(Total_emission_Baltimore$Emissions, by = list(year=Total_emission_Baltimore$year), FUN = sum)
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(Total_emission_by_year_Baltimore$year, Total_emission_by_year_Baltimore$x, xlab = "Year", ylab = "Total PM2.5 Emissions Baltimore", pch = 20)
+dev.copy(png, file = "plot1.png")
+dev.off()
