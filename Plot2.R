ee37ec24e71a6cefbd9f0a607a3c029d3805c26c
vhcg77 <vhcg77@gmail.com>
2018-02-26 01:43:40 -0300

    Fifth commit

--- Plot2.R
+++ Plot2.R
@@ -11,5 +11,5 @@ Total_emission_by_year_Baltimore <- aggregate(Total_emission_Baltimore$Emissions
 
 Sys.setlocale(category = "LC_ALL", locale = "english")
 plot(Total_emission_by_year_Baltimore$year, Total_emission_by_year_Baltimore$x, xlab = "Year", ylab = "Total PM2.5 Emissions Baltimore", pch = 20)
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot2.png")
 dev.off()
---
+++ Plot3.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+
+# Load the data
+NEI <- readRDS("c:/2018/ejemplos/pm2.5/summarySCC_PM25.rds")
+SCC <- readRDS("c:/2018/ejemplos/pm2.5/Source_Classification_Code.rds")
+
+head(NEI)
+head(SCC)
+
+Total_emission_Baltimore <- subset(NEI, fips == "24510")
+Emission_by_type_Baltimore <- aggregate(Total_emission_Baltimore$Emissions, by = list(type=Total_emission_Baltimore$type, year=Total_emission_Baltimore$year), FUN = sum)
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+qplot(year, x, data=Emission_by_type_Baltimore, color=type, geom=c("line"), xlab = "year", ylab = "Total PM2.5 Baltimore")
+dev.copy(png, file = "plot3.png")
+dev.off()
