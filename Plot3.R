a7a5b90f705a7bbb7df934e74b3de53eea786de6
vhcg77 <vhcg77@gmail.com>
2018-02-26 03:01:20 -0300

    Seventh Commit

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Load the data
 NEI <- readRDS("c:/2018/ejemplos/pm2.5/summarySCC_PM25.rds")
 SCC <- readRDS("c:/2018/ejemplos/pm2.5/Source_Classification_Code.rds")
@@ -5,21 +7,11 @@ SCC <- readRDS("c:/2018/ejemplos/pm2.5/Source_Classification_Code.rds")
 head(NEI)
 head(SCC)
 
-# Find index where text "Coal" appears in the SCC dataframe
-indexes <- grepl("Coal", SCC$EI.Sector)
-
-# Pick up the SCC values where text "Coal" appears
-SCC_values <- SCC$SCC[indexes]
-SCC_values <- strsplit(as.character(SCC_values), " \\(")
-
-# Select rows where PM2.5 source is coal
-Coal_emission <- NEI[is.element(NEI$SCC, SCC_values),]
-
-# We compute the total emission by year where PM2.5 source is coal
-Coal_emission_by_year <- aggregate(Coal_emission$Emissions, by = list(year=Coal_emission$year), FUN = sum)
-
+Total_emission_Baltimore <- subset(NEI, fips == "24510")
+Emission_by_type_Baltimore <- aggregate(Total_emission_Baltimore$Emissions, by = list(type=Total_emission_Baltimore$type, year=Total_emission_Baltimore$year), FUN = sum)
 
 Sys.setlocale(category = "LC_ALL", locale = "english")
-plot(Coal_emission_by_year$year, Coal_emission_by_year$x, xlab = "Year", ylab = "Coal PM2.5 Emissions", pch = 20)
+qplot(year, x, data=Emission_by_type_Baltimore, color=type, geom=c("line"), xlab = "year", ylab = "Total PM2.5 Baltimore")
 dev.copy(png, file = "plot3.png")
 dev.off()
+
---
+++ Plot4.R
@@ -0,0 +1,25 @@
+# Load the data
+NEI <- readRDS("c:/2018/ejemplos/pm2.5/summarySCC_PM25.rds")
+SCC <- readRDS("c:/2018/ejemplos/pm2.5/Source_Classification_Code.rds")
+
+head(NEI)
+head(SCC)
+
+# Find index where text "Coal" appears in the SCC dataframe
+indexes <- grepl("Coal", SCC$EI.Sector)
+
+# Pick up the SCC values where text "Coal" appears
+SCC_values <- SCC$SCC[indexes]
+SCC_values <- strsplit(as.character(SCC_values), " \\(")
+
+# Select rows where PM2.5 source is coal
+Coal_emission <- NEI[is.element(NEI$SCC, SCC_values),]
+
+# We compute the total emission by year where PM2.5 source is coal
+Coal_emission_by_year <- aggregate(Coal_emission$Emissions, by = list(year=Coal_emission$year), FUN = sum)
+
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(Coal_emission_by_year$year, Coal_emission_by_year$x, xlab = "Year", ylab = "Coal PM2.5 Emissions", pch = 20)
+dev.copy(png, file = "plot4.png")
+dev.off()
